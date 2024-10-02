f692b456df188afec2e56228c199f030a9256656
Ahmed-18A <ahmed.abu.21418@gmail.com>
2024-10-02 16:42:29 +0300

    second

--- app/src/main/java/com/example/myapplication2/MainActivity.java
+++ app/src/main/java/com/example/myapplication2/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.myapplication2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+private TextView etWelcome;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        etWelcome=findViewById(R.id.text1);
+    }
+    public void change_text(View view){
+        etWelcome.setText("this is my first app");
+    }
+    public void finishApp(View view){
+        finish();
+        System.exit(0);
     }
 }
